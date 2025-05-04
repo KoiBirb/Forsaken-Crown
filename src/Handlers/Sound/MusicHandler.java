32a0133bbe87be3b32f71d34584f3fae306a8bd1
KoiBirb <centralleo474@gmail.com>
2025-05-04 15:58:26 -0400

    Audio files

---
+++ src/Handlers/Sound/MusicHandler.java
@@ -0,0 +1,81 @@
+package Handlers.Sound;
+
+import java.util.Random;
+
+public class MusicHandler {
+
+    public static final Sound music = new Sound();
+    public static final Sound attack = new Sound();
+    public static final Sound footsteps = new Sound();
+    public static final Sound effect = new Sound();
+    public static final Sound falling = new Sound();
+    private static final Random rand = new Random();
+
+    public static void playBackgroundMusic() {
+        playMusic(0);
+    }
+
+    public static void playMusic(int i) {
+        music.setFile(i);
+        music.play();
+        music.loop();
+    }
+
+    public static void hit() {
+        playSoundEffect(rand.nextInt(4), attack);
+    }
+
+    public static void jump() {
+        playSoundEffect(5, effect);
+    }
+
+    public static void land() {
+        playSoundEffect(7, effect);
+    }
+
+    public static void landHard() {
+        playSoundEffect(6, effect);
+    }
+
+    public static void heavyAttack() {
+        playSoundEffect(4, attack);
+    }
+
+    public static void hitTile() {
+        playSoundEffect(9, attack);
+    }
+
+    public static void footsteps() {
+        if (!footsteps.isPlaying()) {
+            footsteps.setFile(8);
+            footsteps.play();
+            footsteps.loop();
+        }
+    }
+
+    public static void falling() {
+        if (!falling.isPlaying()) {
+            falling.setFile(10);
+            falling.play();
+            falling.loop();
+        }
+    }
+
+    public static void stopFalling() {
+        if (falling.isPlaying()) {
+            falling.stop();
+        }
+    }
+
+    public static void stopFootsteps() {
+        if (footsteps.isPlaying()) {
+            footsteps.stop();
+        }
+    }
+
+    private static void playSoundEffect(int i, Sound sound) {
+        if (sound.setFile(i)) {
+            sound.play();
+        }
+    }
+}
---
+++ src/Handlers/Sound/Sound.java
@@ -0,0 +1,58 @@
+package Handlers.Sound;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class Sound {
+
+    private Clip clip;
+    private final ArrayList<URL> soundURL = new ArrayList<>();
+
+    public Sound() {
+
+        // player sounds
+        soundURL.add(getClass().getResource("/Audio/Player/sword_1.wav"));
+        soundURL.add(getClass().getResource("/Audio/Player/sword_2.wav"));
+        soundURL.add(getClass().getResource("/Audio/Player/sword_3.wav"));
+        soundURL.add(getClass().getResource("/Audio/Player/sword_4.wav"));
+        soundURL.add(getClass().getResource("/Audio/Player/heavy_sword.wav"));
+        soundURL.add(getClass().getResource("/Audio/Player/jump.wav"));
+        soundURL.add(getClass().getResource("/Audio/Player/land_hard.wav"));
+        soundURL.add(getClass().getResource("/Audio/Player/land.wav"));
+        soundURL.add(getClass().getResource("/Audio/Player/footsteps.wav"));
+        soundURL.add(getClass().getResource("/Audio/Player/sword_hit_tile.wav"));
+        soundURL.add(getClass().getResource("/Audio/Player/falling.wav"));
+    }
+    public boolean setFile(int i){
+        try{
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL.get(i));
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+            return true;
+        } catch (Exception e){
+            System.out.println("Error with sound file.");
+            return false;
+        }
+    }
+
+    public boolean isPlaying() {
+        return clip != null && clip.isRunning();
+    }
+
+    public void play(){
+        clip.start();
+    }
+
+    public void loop(){
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stop(){
+        clip.stop();
+    }
+
+
+}
