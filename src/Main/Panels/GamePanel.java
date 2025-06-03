5efb1ee9152aeeacccecb2041bed0fc405a5db3e
KoiBirb <centralleo474@gmail.com>
2025-06-03 13:08:02 -0400

    Fixed map

--- src/Main/Panels/GamePanel.java
+++ src/Main/Panels/GamePanel.java
@@ -74,8 +74,8 @@ public class GamePanel extends JPanel implements Runnable{
      */
     public void setupGame() {
         this.requestFocusInWindow();
-        player = new Player(new Vector2(100,100));
-//        player = new Player(new Vector2(2300,2500));
+//        player = new Player(new Vector2(100,100));
+        player = new Player(new Vector2(2300,2500));
         ui = new UIManager(player, true);
         backgroundMusic = new BackgroundMusicHandler();
         EnemySpawnHandler.setup();
