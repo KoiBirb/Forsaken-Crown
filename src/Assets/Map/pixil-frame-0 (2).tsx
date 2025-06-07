ad9265a695945029d0a1d39749f6d409b0231a99
KoiBirb <centralleo474@gmail.com>
2025-06-07 19:36:33 -0400

    File cleanup

--- src/Assets/Map/pixil-frame-0 (2).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="pixil-frame-0 (2)" tilewidth="16" tileheight="16" tilecount="36" columns="6">
- <image source="../../../../../../../Downloads/pixil-frame-0 (2).png" width="100" height="100"/>
-</tileset>
