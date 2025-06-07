e4145dfe37052a0235da4a59aa436c006e5f70e7
Ben <339710097@gotvdsb.ca>
2025-06-07 14:54:41 -0400

    Updated Map

--- src/Assets/Images/Tilesets/Map/pixil-frame-0 (5).tsx
+++ src/Assets/Images/Tilesets/Map/pixil-frame-0 (5).tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="pixil-frame-0 (5)" tilewidth="16" tileheight="16" tilecount="36" columns="6">
- <image source="pixil-frame-0 (5).png" width="100" height="100"/>
+<tileset version="1.10" tiledversion="1.11.2" name="pixil-frame-0 (5)" tilewidth="16" tileheight="16" tilecount="81" columns="9">
+ <image source="pixil-frame-0 (5).png" width="144" height="144"/>
 </tileset>
