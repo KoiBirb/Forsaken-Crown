15b9f183b0cbb0338381fbf1bb8b2096cb657cc9
KoiBirb <centralleo474@gmail.com>
2025-06-10 12:58:55 -0400

    Map fixes

---
+++ src/Assets/Map/DARK Edition Tileset No background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="DARK Edition Tileset No background" tilewidth="16" tileheight="16" tilecount="288" columns="18">
+ <image source="../Images/Tilesets/Map/DARK Edition Tileset No background.png" trans="1bf700" width="288" height="256"/>
+</tileset>
--- src/Main/Panels/GamePanel.java
+++ src/Main/Panels/GamePanel.java
@@ -84,7 +84,7 @@ public class GamePanel extends JPanel implements Runnable{
      */
     public void setupGame() {
         this.requestFocusInWindow();
-        player = new Player(new Vector2(100,100));
+        player = new Player(new Vector2(100,400));
         ui = new UIManager(player, true);
         checkpointManager = new CheckpointManager();
         EnemySpawnHandler.setup();
